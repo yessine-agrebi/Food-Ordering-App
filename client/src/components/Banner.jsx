5bb8d18f33a5e84b5a85ac245b9863504903bf73
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-07-28 14:39:02 +0100

    banner

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Button from './elements/Button'
 
 const Banner = () => {
   return (
-    <div>
-      
+    <div className='banner w-full md:w-2/3 px-7 mx-auto relative flex items-center justify-between'>
+      <div className='banner-description w-full md:w-1/2 p-3'>
+        <h2 className="mb-6 text-4xl font-bold text-white">
+        Food Ordering Made Easy
+        </h2>
+        <p classname="font-semibold text-lg text-red-600 py-2">
+            GetStarted Today!
+        </p>
+        <div className="btn-container">
+            <Button>Order Now</Button>
+            <a href="/menu" className='text-yellow-400 hover:text-yellow-500 font-bold text-decoration-line px-3'>
+                See Menu
+            </a>
+        </div>
+      </div>
+      <div className="banner-image w-full md:w-1/2 p-3 flex justify-end">
+        <img src={require('../assets/images/pizza_banner.png')} alt="Hero" className='max-h-95' />
+      </div>
     </div>
   )
 }
---
+++ client/src/components/elements/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({children}) => {
+  return (
+    <Button>
+        {children}
+    </Button>
+  )
+}
+
+export default Button;
